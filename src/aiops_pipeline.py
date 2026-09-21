8726dd32b4eca80368a5e1ae7eac8c7d96f639ae
Kanak-goyal0107 <goyalkanak0107@gmail.com>
2026-09-21 05:54:11 +0000

    Fix anomaly detection and event flow

--- src/aiops_pipeline.py
+++ src/aiops_pipeline.py
@@ -15,14 +15,18 @@ def run_pipeline(file_path):
     data = load_data(file_path)
 
     # INTENTIONAL ASSESSMENT ISSUE #2
-    producer_topic = EventTopic("service-events")
+    #producer_topic = EventTopic("service-events")
 
-    detector = AnomalyDetector()
-    producer = EventProducer(producer_topic)
+    #detector = AnomalyDetector()
+    #producer = EventProducer(producer_topic)
 
     # INTENTIONAL ASSESSMENT ISSUE #3
-    consumer_topic = EventTopic("anomaly-events")
-    consumer = EventConsumer(consumer_topic)
+    #consumer_topic = EventTopic("anomaly-events")
+    #consumer = EventConsumer(consumer_topic)
+    topic = EventTopic("anomaly-events")
+    detector = AnomalyDetector()
+    producer = EventProducer(topic)
+    consumer = EventConsumer(topic)
 
     detected_events = []
 
